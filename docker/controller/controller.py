6f221c7c0e05b3de5214dc3dfcddbdf898fd83b5
Pablo Toledo <jptgjuanpablo@gmail.com>
2023-11-28 10:50:46 +0100

    Update dependencies and add k8s-job-worker.yaml

--- docker/controller/controller.py
+++ docker/controller/controller.py
@@ -1,16 +1,26 @@
 import os
+import time
 from githubutilsapi import GithubUtilsApi
 from kubernetes import client, config
+import yaml
+from queue import Queue
 
 # K8s configuration
 MAX_REPLICAS = int(os.environ.get("ADO_MAX_SELF_HOSTED_AGENTS", 5))
-NAMESPACE = 'devops-k8s-ns'
-LABEL_SELECTOR = "role=ado-agent"
+NAMESPACE = 'dependency-check-devsecops-ns'
+LABEL_SELECTOR = "role=dcheck-node"
+
+# Load K8s configuration
 config.load_incluster_config()
+
+# Create the K8s API clients
 v1 = client.AppsV1Api()
+batch_v1 = client.BatchV1Api()
 
+# Create the queue for pending jobs
+pending_jobs_queue = Queue()
 
-
+#### GITHUB API ####
 
 gh_user = os.environ.get('GITHUB_USER')
 gh_token = os.environ.get('GITHUB_TOKEN')
@@ -25,24 +35,75 @@ github = GithubUtilsApi(user=gh_user, token=gh_token, github_url=gh_api_url, git
 def get_plain_repo_list():
     return github.list_repositories_all(organization)
 
+#### K8S API ####
 
-get_plain_repo_list()
+def create_k8s_job(repo_name, repo_url):
+    # Load the kubeconfig file
+    config.load_kube_config()
 
+    # Read the job description from the YAML file
+    with open("/app/k8s-job-worker.yaml") as file:
+        job_yaml = yaml.safe_load(file)
 
-def create_k8s_job():
-    def load_yaml_file(relative_path):
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        yaml_path = os.path.join(script_dir, relative_path)
-        
-        with open(yaml_path, 'r') as file:
-            return yaml.safe_load(file)
-        
-    api_instance = client.BatchV1Api()
-    job_spec = load_yaml_file("job-agent.yaml")
-    job = client.V1Job(
-        api_version="batch/v1",
-        kind="Job",
-        metadata=client.V1ObjectMeta(generate_name="ado-agent-"),
-        spec=job_spec["spec"]
+    # Modify the environment variables
+    for env_var in job_yaml['spec']['template']['spec']['containers'][0]['env']:
+        if env_var['name'] == 'GITHUB_REPO_NAME':
+            env_var['value'] = repo_name
+        elif env_var['name'] == 'GITHUB_URL':
+            env_var['value'] = repo_url
+
+    # Create the Job
+    k8s_client = client.BatchV1Api()
+    job = k8s_client.create_namespaced_job(
+        body=job_yaml,
+        namespace="default"
     )
-    api_instance.create_namespaced_job(namespace=NAMESPACE, body=job)
+    print("Job created")
+
+def get_active_jobs_count():
+    """
+    Return the number of active jobs.
+    """
+    jobs = batch_v1.list_namespaced_job(NAMESPACE, label_selector=LABEL_SELECTOR)
+    active_jobs = [job for job in jobs.items if job.status.active]
+    return len(active_jobs)
+
+def clean_up_completed_jobs():
+    """
+    Remove the completed jobs.
+    """
+    completed_jobs = batch_v1.list_namespaced_job(
+        namespace=NAMESPACE, 
+        label_selector=LABEL_SELECTOR
+    ).items
+
+    for job in completed_jobs:
+        if job.status.succeeded or job.status.failed:
+            batch_v1.delete_namespaced_job(
+                name=job.metadata.name,
+                namespace=NAMESPACE,
+                body=client.V1DeleteOptions(propagation_policy='Foreground')
+            )
+
+def process_repositories():
+    """
+    Run the process to create the jobs.
+    """
+    repos = get_plain_repo_list()
+    for repo in repos:
+        pending_jobs_queue.put(repo)
+
+    while not pending_jobs_queue.empty():
+        active_jobs_count = get_active_jobs_count()
+        
+        if active_jobs_count < MAX_REPLICAS:
+            repo = pending_jobs_queue.get()
+            create_k8s_job(repo['name'], repo['html_url'])
+        else:
+            print("Waiting available resources...")
+            time.sleep(10)  # Await 10 seconds
+
+        clean_up_completed_jobs()
+
+# Start the job creation process
+process_repositories()
